19dbcab553ad533d3ea20d6df47c4e54e408e37f
Виктория Разнодумова <raznodumova@gmail.com>
2026-02-16 23:44:14 +0300

    working version with dockerfile and docker-compose.yml

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import java.math.BigInteger;
 
 public interface UserRepository extends MongoRepository<User, Long> {
 }
